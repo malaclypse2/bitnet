a4b143a87af1eb8d98dc3472b0d21e80b45a6ded
malaclypse2 <malaclypse2@gmail.com>
2022-02-06 12:36:10 -0500

    proof of concept

--- scripts/scrap.js
+++ scripts/scrap.js
@@ -1,11 +1,18 @@
 /** @typedef {import('/scripts/index.js').NS} NS */
 
-import { getServerNames } from '/scripts/bit-lib.js';
+import { hashCode } from './helpers.js';
 
 /**@param {NS} ns */
 export async function main(ns) {
-	let servernames = getServerNames(ns);
-	for(let nm of servernames) ns.tprint(nm);
+	let items = [
+		{ interval: 32000, name: "hacknet-upgrade-manager.js", shouldRun: true, args: () => ["-c", "--max-payoff-time", "4h", "--max-spend", ns.getServerMoneyAvailable("home") * 0.1] },
+        { interval: 33000, name: "hacknet-upgrade-manager.js", shouldRun: true, args: () => ["-c", "--max-payoff-time", "8h", "--max-spend", ns.getServerMoneyAvailable("home") * 0.01] },
+	]
+	
+	ns.tprint(hashToolDefinition(items[0]));
+	ns.tprint(hashToolDefinition(items[1]));
 
 }
+
+const hashToolDefinition = s => hashCode(s.name + JSON.stringify(s.args || []));
 
